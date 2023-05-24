903a724149e82bbe4378e7c2708692b51f902be7
Craig Gordon <craigmgordon@gmail.com>
2023-05-24 16:58:00 +0100

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+resource "google_storage_bucket" "tfstate" {
+    project  = var.project_id
+    name     = "${var.project_id}-state"
+    location    = "US"
+    uniform_bucket_level_access = true
+}
+terraform {
+    backend "gcs" {
+    bucket  = "roimb-devops-7-dev-state"
+    prefix  = "terraform/state"
+    }
+}
